50d266447330b77e3acdcf3f222c8def71072b59
Maria Golovatenko <pikch4y@yandex.com>
2025-12-09 15:59:27 +0300

    Updates

---
+++ .eslintrc.cjs
@@ -0,0 +1,60 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true
+  },
+  extends: ['plugin:react/recommended', 'prettier'],
+  plugins: ['react', '@typescript-eslint', 'react-hooks', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'react/display-name': 'off',
+    'no-console': 'off',
+    'react/react-in-jsx-scope': 'off',
+    quotes: ['off'],
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.tsx'] }
+    ],
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'off',
+    'react/require-default-props': 'off',
+    'react/prop-types': 'off',
+    'react/function-component-definition': 'off',
+    'no-shadow': 'off',
+    'no-nested-ternary': 'off',
+    'import/extensions': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'no-underscore-dangle': 'off',
+    semi: ['error', 'never'],
+    'max-len': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'no-param-reassign': 'off',
+    'no-undef': 'off',
+    'react/no-array-index-key': 'off'
+  },
+  globals: {
+    __IS_DEV__: true,
+    __API__: true,
+    __PROJECT__: true
+  },
+  overrides: [
+    {
+      files: ['**/src/**/*.{test,stories}.{ts,tsx}'],
+      rules: {
+        'max-len': 'off'
+      }
+    }
+  ]
+}
