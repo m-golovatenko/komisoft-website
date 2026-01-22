b717614330ab65d67100b47a4a2b1690e7f50314
m_golovatenko <pikch4y@yandex.com>
2026-01-22 11:30:40 +0300

    hotfix: fix some bugs

--- src/components/layout/Header/header.config.ts
+++ src/components/layout/Header/header.config.ts
@@ -3,8 +3,7 @@ import { SECTIONS } from '@/const/sections'
 export const NAV_LINKS = [
   { label: 'О нас', section: SECTIONS.about },
   { label: 'Проекты', section: SECTIONS.projects },
-  { label: 'Услуги', section: SECTIONS.services },
-  { label: 'Карьера', section: SECTIONS.CTA }
+  { label: 'Услуги', section: SECTIONS.services }
 ] as const
 
 export const LANGUAGES = ['RU', 'EN'] as const
